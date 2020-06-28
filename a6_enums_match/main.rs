31c9ee4ee0b19f406baecf5ae1a249f5b70196a3
yangminyu <yangminyu@gosun.com>
2020-06-28 11:52:30 +0800

    添加enum和match语法

--- a6_enums_match/main.rs
+++ a6_enums_match/main.rs
@@ -94,17 +94,17 @@ enum Message {
 }
 
 impl Message {
-  fn call(&self) {
-    println!("enum method:: ===message is {:?}", self);
-  }
+    fn call(&self) {
+        println!("enum method:: ===message is {:?}", self);
+    }
 }
 
 // ============== Option enum 查看
 
-
-
 //====================================================== match语法
-// rust中没有switch语法
+// rust中没有switch语法, 但是有match语法，
+// match可以匹配字面值，变量名称，通配符和其他值
+//
 
 /*
  match 枚举类实例 {
@@ -113,6 +113,35 @@ impl Message {
     ...
 }
 */
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        // 也可以部分匹配
+        Coin::Quarter(state) => {
+            println!("====> State quarter from {:?}!", state);
+            25
+        }
+    }
+}
 
 fn func_match() {
     enum Book {
@@ -147,7 +176,30 @@ fn other_match() {
         "abc" => println!("Yes"),
         _ => println!("others"),
     }
+
+    let some_u8 = 0u8;
+    match some_u8 {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // () 空的tuple， 在这里什么也不会发生
+    }
 }
+
+fn match_option() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("====> option {:?}, {:?}", six, none);
+}
+
 fn main() {
     book1();
     book2();
@@ -155,6 +207,8 @@ fn main() {
     book3();
     let m = Message::Write(String::from("hello"));
     m.call();
+    value_in_cents(Coin::Quarter(UsState::Alaska));
     func_match();
     other_match();
+    match_option();
 }
--- a6_enums_match/option_enum.rs
+++ a6_enums_match/option_enum.rs
@@ -6,13 +6,16 @@
  * Rust 在语言层面彻底不允许空值 null 的存在，但无奈null 可以高效地解决少量的问题，
  * 所以 Rust 引入了 Option 枚举类：
  *
+ * 如果没有包含在Option中的类型不需要担心null值，也不需要处理，
+ * 如果声明了Option则必须判断null值，并且需要手动处理包含的值。
+ * 一般可以通过match操作来处理。
+ *
  */
 
 // enum Option<T> {
 //    Some(T),
 //    None,
 // }
-
 
 // 如果你的变量刚开始是空值，你体谅一下编译器，
 // 它怎么知道值不为空的时候变量是什么类型的呢？
@@ -23,11 +26,25 @@ fn option_func() {
     match opt {
         Option::Some(something) => {
             println!("{}", something);
-        },
+        }
         Option::None => {
             println!("opt is nothing");
         }
     }
+}
+
+// Opton<T> 定义了许多方法可以使用
+fn define_optoin() {
+    let x: Option<u32> = Some(20);
+    assert_eq!(x.is_some(), true);
+    let x: Option<u32> = None;
+    assert_eq!(x.is_none(), true);
+
+    // Some可以省略类型，编译器会自动判断
+    let some_nubmer = Some(5);
+    let some_string = Some("a string");
+    // 如果使用None的时候，必须明确标识类型
+    let absent_number: Option<i32> = None;
 }
 
 // Option 是一种特殊的枚举类，它可以含值分支选择：
@@ -35,40 +52,49 @@ fn option_match() {
     let t = Some(64);
     match t {
         Some(64) => println!("Yes"),
-        _ => println!("No"),
+        _ => (),
+    }
+
+    // 如果用if let
+    if let Some(64) = t {
+        println!("Yes");
     }
 }
 
 // if let 语法
+// if let 语法以一种简洁的方式匹配一种情况，而忽略其他情况。
 // if let 语法可以认为是之区分两种情况的 match 语句的"语法糖".
 // 对于枚举类依然适用：
 fn if_let() {
     let i = 0;
     match i {
         0 => println!("zero"),
-        _ => {},
+        _ => println!("none"),
     }
 
+    // if let 还可以加一个else分支表示 _
     if let 0 = i {
         println!("zero");
+    } else {
+        println!("none");
     }
 
     enum Book {
         Papery(u32),
-        Electronic(String)
+        Electronic(String),
     }
     let book = Book::Electronic(String::from("url"));
+    // 可以加一个else分支
     if let Book::Papery(index) = book {
         println!("Papery {}", index);
     } else {
         println!("Not papery book");
     }
-
 }
-
 
 fn main() {
     option_func();
+    define_optoin();
     option_match();
     if_let();
 }
