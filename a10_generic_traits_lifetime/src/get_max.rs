932967f93b13f126f5ca5ca65a229834dd2e08d7
yangminyu <yangminyu@gosun.com>
2020-07-02 18:28:35 +0800

    update a10 trait

--- a10_generic_traits_lifetime/src/get_max.rs
+++ a10_generic_traits_lifetime/src/get_max.rs
@@ -1,4 +1,3 @@
-
 // Tip: 由于需要声明 compare 函数的第二参数必须与实现该特性的类型相同，
 // 所以 Self （注意大小写）关键字就代表了当前类型（不是实例）本身。
 trait Comparable {
@@ -19,13 +18,46 @@ fn max<T: Comparable>(array: &[T]) -> &T {
 
 impl Comparable for f64 {
     fn compare(&self, object: &f64) -> i8 {
-        if &self > &object { 1 }
-        else if &self == &object { 0 }
-        else { -1 }
+        if &self > &object {
+            1
+        } else if &self == &object {
+            0
+        } else {
+            -1
+        }
     }
+}
+
+//================================= 官网 =========================
+// fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+//     let mut largest = list[0];
+//     for &item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
+// 返回&T
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+    for &item in list {
+        if item > *largest {
+            largest = &item;
+        }
+    }
+    largest
 }
 
 fn main() {
     let arr = [1.0, 3.0, 5.0, 4.0, 2.0];
     println!("maximum of arr is {}", max(&arr));
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
---
+++ a10_generic_traits_lifetime/src/trait.rs
@@ -0,0 +1,126 @@
+pub mod news {
+    // 定义trait
+    pub trait Summary {
+        // fn summarize(&self) -> String;
+        fn summarize_author(&self) -> String;
+        // 默认实现
+        fn summarize(&self) -> String {
+            String::from("Read more...");
+            // 默认实现可以调用其他方法
+            format!("(Read more from{}...)", self.summarize_author())
+        }
+        // 使用默认
+        // imple Summary for Tweet {}
+
+        // 可以只实现非默认方法
+        // impl Summary from Tweet {
+        //     fn summarize_author(&self) -> String {
+        //         format!("@{}", self.username)
+        //     }
+        // }
+    }
+
+    pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+    }
+
+    // 实现
+    // 注意：只有trait或者type对于crate是本地的时候，我们才能在type上实现trait。
+    // Vec<T>可以实现Summary, 不能实现Vec<T>实现Display
+    impl Summary for NewsArticle {
+        fn summarize(&self) -> String {
+            format!("{}, by {} ({})", self.headline, self.author, self.location)
+        }
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: bool,
+        pub retweet: bool,
+    }
+
+    impl Summary for Tweet {
+        fn summarize(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
+    }
+}
+
+// 这里必须要把Summary引入，不然无法运行方法
+use news::{Summary, Tweet};
+
+// ========================= 方法参数 ================
+// imple Trait语法更直观，但是对于多个参数来说Trait Bound语法更加简介
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// 可以使用 Trait Bound语法简化上面的方法， 其实是impl trait的语法糖。
+fn notify_1<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// 使用 + 指定多个trait
+fn notify_2(item: &(impl Summary + Display)) {}
+fn notify_3<T: Summary + Display>(item: &T) {}
+
+// 使用where子句使结构更加清晰
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) -> i32 {
+    10
+}
+fn some_function<T, U>(t: &T, u: &U) -> i32
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
+    10
+}
+
+// 返回Trait
+// 返回类型的功能在闭包和迭代器中特别有用
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+// 但是只能返回同一种具体类类型
+fn returns_summarizable_1(switch: bool) -> impl Summary {
+    if switch {
+        NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        }
+    } else {
+        Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        }
+    }
+}
+
+fn main() {
+    returns_summarizable();
+    // returns_summarizable_1();
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
