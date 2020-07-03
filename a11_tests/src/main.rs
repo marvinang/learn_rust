5d7573fb07088db3c20323781c27c8a593c0a13b
yangminyu <yangminyu@gosun.com>
2020-07-03 18:27:12 +0800

    update a11

--- a11_tests/src/main.rs
+++ a11_tests/src/main.rs
@@ -1,3 +1,24 @@
+/*
+
+rust使用test属性标记函数成为一个测试案例。
+属性是关于一段rust代码的元数据。
+
+
+
+
+*/
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, test!");
 }
