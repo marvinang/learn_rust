058d6a764663dbb4ef318a3ac1602b0ae8ea8e08
marvinang <marvin_yang@yeah.net>
2020-07-05 20:30:02 +0800

    update a12 command line

---
+++ a12_command_line/src/main.rs
@@ -0,0 +1,22 @@
+/*
+ *
+ * 一个命令行程序
+ */
+
+use std::env;
+use std::process;
+
+use a12_command_line::{self, Config};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = a12_command_line::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
