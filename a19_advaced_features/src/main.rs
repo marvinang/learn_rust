a45ae031dae0d6b2cad7108e104c180d1a9eb4ef
marvinang <marvin_yang@yeah.net>
2020-07-11 23:42:44 +0800

    update a19

--- a19_advaced_features/src/main.rs
+++ a19_advaced_features/src/main.rs
@@ -1,3 +1,13 @@
+/*
+ * 高级特性
+
+1. 不安全 Rust：用于当需要舍弃 Rust 的某些保证并负责手动维持这些保证
+2. 高级 trait：与 trait 相关的关联类型，默认类型参数，完全限定语法（fully qualified syntax），
+   超（父）trait（supertraits）和 newtype 模式
+3. 高级类型：关于 newtype 模式的更多内容，类型别名，never 类型和动态大小类型
+4. 高级函数和闭包：函数指针和返回闭包
+5. 宏：定义在编译时定义更多更多代码的方式
+ *
+ */
 fn main() {
-    println!("Hello, world!");
 }
