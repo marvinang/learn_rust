be65553988a02f78ec4b2c2fb6bc40efc2f0acde
yangminyu <yangminyu@gosun.com>
2020-07-21 14:49:35 +0800

    添加vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {
+  region      = "cn-north-1"
+  domain_name = "zysshsu"
+  access_key  = "CPBVU9YVMZHG5MC1T2OA"
+  secret_key  = "SLB8fM1bYTb1zP04kWSeXcWMQqozqQ8EUxaumalt"
+}
+
+# Create a VPC
+resource "huaweicloud_vpc_v1" "example" {
+  name = "my_vpc"
+  cidr = "192.168.0.0/16"
+}
